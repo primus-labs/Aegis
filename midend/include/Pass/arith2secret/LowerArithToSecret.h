9ff1ea72a212a6edf264ab78e20040e974aa6669
cyf <yuanfeng@padolabs.org>
2025-02-19 14:51:51 +0800

    Added arith2secret pass, not implemented

---
+++ midend/include/Pass/arith2secret/LowerArithToSecret.h
@@ -0,0 +1,23 @@
+#ifndef PASS_ARITH2SECRET_LOWERARITHTOSECRET_H
+#define PASS_ARITH2SECRET_LOWERARITHTOSECRET_H
+
+
+#include "mlir/IR/BuiltinOps.h"
+#include "mlir/Pass/Pass.h"
+#include "mlir/Transforms/DialectConversion.h"
+#include "Dialect/Secret/SecretDialect.h"
+
+
+struct LowerArithToSecretPass : public mlir::PassWrapper<LowerArithToSecretPass, mlir::OperationPass<mlir::ModuleOp>>
+{
+    void getDependentDialects(mlir::DialectRegistry &registry) const override;
+
+    void runOnOperation() override;
+
+    mlir::StringRef getArgument() const final
+    {
+        return "arith2secret";
+    }
+};
+
+#endif
---
+++ midend/lib/Pass/arith2secret/LowerArithToSecret.cpp
@@ -0,0 +1,30 @@
+#include "Pass/arith2secret/LowerArithToSecret.h"
+#include <iostream>
+#include <memory>
+#include "mlir/Dialect/Affine/IR/AffineOps.h"
+#include "mlir/Dialect/Affine/Utils.h"
+#include "mlir/Dialect/Affine/LoopUtils.h"
+#include "mlir/Dialect/Func/IR/FuncOps.h"
+#include "mlir/Dialect/SCF/IR/SCF.h"
+#include "mlir/Dialect/Tensor/IR/Tensor.h"
+#include "mlir/Dialect/Arith/IR/Arith.h"
+#include "mlir/Dialect/MemRef/IR/MemRef.h"
+
+using namespace mlir;
+using namespace aegis;
+
+
+void LowerArithToSecretPass::getDependentDialects(mlir::DialectRegistry &registry) const 
+{
+    registry.insert<arith::ArithDialect>();
+    registry.insert<affine::AffineDialect>();
+    registry.insert<func::FuncDialect>();
+    registry.insert<secret::SecretDialect>();
+    registry.insert<scf::SCFDialect>();
+    registry.insert<memref::MemRefDialect>();
+}
+
+
+void LowerArithToSecretPass::runOnOperation() {
+    //TODE
+}
