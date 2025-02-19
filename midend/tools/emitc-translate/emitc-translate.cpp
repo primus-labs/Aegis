fbc562a88034569ee8038238c1945ca2d4011c1e
cyf <yuanfeng@padolabs.org>
2025-02-19 17:37:52 +0800

    added emitc to cpp tools

---
+++ midend/tools/emitc-translate/emitc-translate.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "mlir/Dialect/EmitC/IR/EmitC.h"
+#include "mlir/InitAllDialects.h"
+#include "mlir/InitAllTranslations.h"
+#include "mlir/Tools/mlir-translate/MlirTranslateMain.h"
+
+using namespace mlir;
+
+int main(int argc, char **argv)
+{
+    mlir::MLIRContext context;
+
+    mlir::DialectRegistry registry;
+    registry.insert<emitc::EmitCDialect>();
+    // Uncomment the following to include *all* MLIR Core dialects, or selectively
+    // include what you need like above. You only need to register dialects that
+    // will be *parsed* by the tool, not the one generated
+    registerAllDialects(registry);
+
+    registerAllTranslations();
+    context.loadAllAvailableDialects();
+
+    return failed(mlir::mlirTranslateMain(argc, argv, "EmitC Translation Tool"));
+}
