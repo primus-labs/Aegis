b30896dccfe740ed6c5534f06ac778ae90f54026
cyf <yuanfeng@padolabs.org>
2025-02-19 09:41:23 +0800

    update script file

--- scripts/update_dialect_files.sh
+++ scripts/update_dialect_files.sh
@@ -10,22 +10,24 @@ echo "****************************************************"
 cd ../
 mkdir -p build
 cd build
-cmake ../midend/ -DMLIR_DIR=../third_party/llvm-project/build/lib/cmake/mlir/
+# cmake ../midend/ -DMLIR_DIR=../third_party/llvm-project/build/lib/cmake/mlir/
 make mlir-doc
 
 
 echo "****************************************************"
-echo "*************    copy dialect docs     *************"
+echo "*********    copy secret dialect docs     **********"
 echo "****************************************************"
 cd docs/Secret
 echo "copy SecretDialect.md..."
 cat SecretDialect.md SecretOps.md SecretTypes.md > ../../../docs/dev/dialects/SecretDialect.md
 
-#echo "copy FHEDialect.md..."
-#cp FHEDialect.md ../../../docs/dev/dialects/
+echo "****************************************************"
+echo "**********    copy fhe dialect docs     ***********"
+echo "****************************************************"
+cd ../FHE
+echo "copy FHEDialect.md..."
+cat FHEDialect.md FHEOps.md FHETypes.md > ../../../docs/dev/dialects/FHEDialect.md
 
-#echo "copy CKKSDialect.md..."
-#cp CKKSDialect.md ../../../docs/dev/dialects/
 
 echo "copy done."
 
