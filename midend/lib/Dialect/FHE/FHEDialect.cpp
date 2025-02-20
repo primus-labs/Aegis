5752465833c57bf19d083fbe32f87babd848b98f
cyf <yuanfeng@padolabs.org>
2025-02-20 16:36:46 +0800

    implement aegis opt

--- midend/lib/Dialect/FHE/FHEDialect.cpp
+++ midend/lib/Dialect/FHE/FHEDialect.cpp
@@ -17,6 +17,37 @@ namespace aegis {
 namespace fhe {
 
 
+/// simplifies away castop(castop(x)) to x if the types work
+::mlir::OpFoldResult fhe::CastOp::fold(fhe::CastOp::FoldAdaptor adaptor)
+{
+    if (auto m_op = getInput().getDefiningOp<fhe::CastOp>()) {
+        if (m_op.getInput().getType() == getResult().getType())
+            return m_op.getInput();
+        
+        if (auto mm_op = m_op.getInput().getDefiningOp<fhe::CastOp>()) {
+            if (mm_op.getInput().getType() == getResult().getType())
+                return mm_op.getInput();
+        }
+    }
+    else if (getInput().getType() == getResult().getType()) {
+        return getInput();
+    }
+
+    return {};
+}
+
+
+/// simplify rotate(cipher, 0) to cipher
+::mlir::OpFoldResult fhe::RotateOp::fold(fhe::RotateOp::FoldAdaptor adaptor)
+{
+    if (getI() == 0)
+        return getCipher();
+
+    return {};
+}
+
+
+
 /*****************************************************************************/
 // FHE dialect.
 // Dialect construction: there is one instance per context and it registers its
--- midend/tools/aegiscompiler/main.cpp
+++ midend/tools/aegiscompiler/main.cpp
@@ -1,3 +1,77 @@
-int main() {
-    return 0;
+#include <iostream>
+#include "llvm/Support/CommandLine.h"
+#include "llvm/Support/InitLLVM.h"
+#include "llvm/Support/SourceMgr.h"
+#include "llvm/Support/ToolOutputFile.h"
+#include "mlir/Dialect/Arith/IR/Arith.h"
+#include "mlir/Dialect/EmitC/IR/EmitC.h"
+#include "mlir/IR/Dialect.h"
+#include "mlir/IR/MLIRContext.h"
+#include "mlir/InitAllDialects.h"
+#include "mlir/InitAllPasses.h"
+#include "mlir/Pass/Pass.h"
+#include "mlir/Pass/PassManager.h"
+#include "mlir/Support/FileUtilities.h"
+#include "mlir/Tools/mlir-opt/MlirOptMain.h"
+#include "Dialect/Secret/SecretDialect.h"
+#include "Dialect/FHE/FHEDialect.h"
+
+
+using namespace mlir;
+using namespace aegis;
+using namespace secret;
+using namespace fhe;
+
+
+void fhePipeline(OpPassManager &manager)
+{
+    // manager.addPass(std::make_unique<UnrollLoopsPass>());
+    manager.addPass(createCanonicalizerPass());
+    manager.addPass(createCSEPass()); // this can greatly reduce the number of operations after unrolling
+}
+
+
+int main(int argc, char **argv)
+{
+    mlir::MLIRContext context;
+    context.enableMultithreading();
+
+    mlir::DialectRegistry registry;
+    registry.insert<SecretDialect>();
+    registry.insert<FHEDialect>();
+    registry.insert<func::FuncDialect>();
+    registry.insert<affine::AffineDialect>();
+    registry.insert<tensor::TensorDialect>();
+    registry.insert<arith::ArithDialect>();
+    registry.insert<emitc::EmitCDialect>();
+    registry.insert<func::FuncDialect>();
+    registry.insert<linalg::LinalgDialect>();
+
+    context.loadDialect<SecretDialect>();
+    context.loadDialect<FHEDialect>();
+    context.loadDialect<func::FuncDialect>();
+    context.loadDialect<affine::AffineDialect>();
+    context.loadDialect<tensor::TensorDialect>();
+    context.loadDialect<arith::ArithDialect>();
+    context.loadDialect<emitc::EmitCDialect>();
+    context.loadDialect<func::FuncDialect>();
+    context.loadDialect<linalg::LinalgDialect>();
+    // Uncomment the following to include *all* MLIR Core dialects, or selectively
+    // include what you need like above. You only need to register dialects that
+    // will be *parsed* by the tool, not the one generated
+    registerAllDialects(registry);
+    context.loadAllAvailableDialects();
+
+    // Uncomment the following to make *all* MLIR core passes available.
+    // This is only useful for experimenting with the command line to compose
+    registerAllPasses();
+
+    registerCanonicalizerPass();
+    affine::registerAffineLoopUnrollPass();
+    registerCSEPass();
+    // PassRegistration<UnrollLoopsPass>();
+
+    PassPipelineRegistration<>("fhe-pass", "Run fhe-level passes", fhePipeline);
+
+    return asMainReturnCode(MlirOptMain(argc, argv, "AEGIS optimizer\n", registry));
 }
