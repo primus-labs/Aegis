c993c65b1fbc7186a49867a9de1a8f5d1a8febb5
cyf <yuanfeng@padolabs.org>
2025-03-19 14:12:00 +0800

    Fixed FheToEmitc pass (Part III) bug and add test cases

--- midend/lib/Pass/FheToEmitc/LowerFheToEmitc.cpp
+++ midend/lib/Pass/FheToEmitc/LowerFheToEmitc.cpp
@@ -320,19 +320,47 @@ public:
 
 
 // Transform memref::LoadOp to emitc::LoadOp.
-class MemrefLoadPattern final : public OpConversionPattern<memref::LoadOp>
+class NativeMemrefLoadPattern final : public OpConversionPattern<memref::LoadOp>
 {
+protected:
+    using OpConversionPattern<memref::LoadOp>::typeConverter;
+
 public:
     using OpConversionPattern<memref::LoadOp>::OpConversionPattern;
 
     LogicalResult matchAndRewrite(memref::LoadOp op, typename memref::LoadOp::Adaptor adaptor, ConversionPatternRewriter &rewriter) const override
     {
-        Value memref = op.getMemref();
+        auto resTy = getTypeConverter()->convertType(op.getType());
+        if (!resTy) {
+            LLVM_DEBUG(llvm::dbgs() << "call convertType fail for op: " << op << ", the op type:" << op.getType() << ".\n");
+            return failure();
+        }
+
+        Value memOperand = op.getMemref();
+        auto operandDestTy = typeConverter->convertType(memOperand.getType());
+        if (!operandDestTy) {
+            LLVM_DEBUG(llvm::dbgs() << "call convertType fail for op: " << memOperand << ", the op type:" << memOperand.getType() << ".\n");
+            return failure();
+        }
+
+        Value newOperand;
+        if (memOperand.getType() != operandDestTy) {
+            newOperand = typeConverter->materializeTargetConversion(rewriter, op.getLoc(), operandDestTy, memOperand);
+            if (auto castOp = mlir::dyn_cast_or_null<fhe::CastOp>(memOperand.getDefiningOp())) {
+                if (auto castOp2 = mlir::dyn_cast_or_null<fhe::CastOp>(castOp.getOperand().getDefiningOp())) {
+                    newOperand = castOp2.getOperand();
+                }
+            }
+        }
+        else {
+            newOperand = memOperand;
+        }
+
         SmallVector<Value, 4> indices(op.getIndices().begin(), op.getIndices().end());
         SmallVector<Value, 8> operands;
-        operands.push_back(memref);
+        operands.push_back(memOperand);
         operands.append(indices.begin(), indices.end());
-        rewriter.replaceOpWithNewOp<emitc::CallOpaqueOp>(op, op.getType(), "Native_Load", operands);
+        auto newOp = rewriter.replaceOpWithNewOp<emitc::CallOpaqueOp>(op, resTy, "Native_Load", operands);
 
         return success();
     }
@@ -375,21 +403,11 @@ public:
             newOperand = memOperand;
         }
 
-        // Convert load index to int attr
         SmallVector<Value, 4> indices(op.getIndices().begin(), op.getIndices().end());
-        SmallVector<Value, 8> ops;
-        ops.push_back(newOperand);
-        for (Value idx : indices) {
-            if (auto constantOp = idx.getDefiningOp<emitc::ConstantOp>()) {
-                ops.push_back(idx);
-            } 
-            else {
-                op.emitError("Non-constant indices are not supported.");
-                return failure();
-            }
-        }
-
-        rewriter.replaceOpWithNewOp<emitc::CallOpaqueOp>(op, destTy, "Load", ops);
+        SmallVector<Value, 8> operands;
+        operands.push_back(newOperand);
+        operands.append(indices.begin(), indices.end());
+        rewriter.replaceOpWithNewOp<emitc::CallOpaqueOp>(op, destTy, "Load", operands);
         
         return success();
     }
@@ -450,8 +468,7 @@ public:
             return failure();
         }
 
-        //auto noneValue = builder.create<emitc::ConstantOp>(loc, builder.getType<mlir::NoneType>(), mlir::Attribute());
-        rewriter.replaceOpWithNewOp<emitc::CallOpaqueOp>(op, destTy, "Alloca", ValueRange());
+        rewriter.replaceOpWithNewOp<emitc::CallOpaqueOp>(op, destTy, "Alloca", ValueRange{});
         return success();
     }
 };
@@ -471,7 +488,7 @@ public:
             return failure();
         }
 
-        rewriter.replaceOpWithNewOp<emitc::CallOpaqueOp>(op, destTy, "Alloc", ValueRange());
+        rewriter.replaceOpWithNewOp<emitc::CallOpaqueOp>(op, destTy, "Alloc", ValueRange{});
         return success();
     }
 };
@@ -494,18 +511,18 @@ public:
 
         auto newMemref = typeConverter->materializeTargetConversion(rewriter, op.getMemref().getLoc(),
                                                                 destMemrefTy, op.getMemref());
-        rewriter.replaceOpWithNewOp<emitc::CallOpaqueOp>(op, TypeRange(), "Dealloc", newMemref);
+        rewriter.replaceOpWithNewOp<emitc::CallOpaqueOp>(op, TypeRange{}, "Dealloc", newMemref);
         return success();
     }
 };
 
 
-
-
 void LowerFheToEmitcPass::getDependentDialects(mlir::DialectRegistry &registry) const
 {
     registry.insert<func::FuncDialect>();
-    registry.insert<mlir::emitc::EmitCDialect>();
+    registry.insert<memref::MemRefDialect>();
+    registry.insert<emitc::EmitCDialect>();
+    registry.insert<fhe::FHEDialect>();
 }
 
 
@@ -514,7 +531,7 @@ void LowerFheToEmitcPass::runOnOperation()
     auto type_converter = TypeConverter();
 
     // Type conversion, convert fhe types to emitc C++ types
-    auto materializeCommon = [](OpBuilder &builder, Type t, ValueRange vs, Location loc) -> std::optional<Value> {
+    auto materializeCommon = [this](OpBuilder &builder, Type t, ValueRange vs, Location loc) -> std::optional<Value> {
         if (auto destTy = mlir::dyn_cast_or_null<emitc::OpaqueType>(t)) {
             assert(!vs.empty() && ++vs.begin() == vs.end() && "currently can only materalize single values");
             auto srcTy = vs.front().getType();
@@ -548,10 +565,23 @@ void LowerFheToEmitcPass::runOnOperation()
                     return std::optional<Value>(builder.create<fhe::CastOp>(loc, destTy, vs));
                 }
             }
+            else if (mlir::dyn_cast_or_null<fhe::PlainVectorType>(srcTy)) {
+                if (destTy.getValue().str() == "std::vector<Plain>") {
+                    return std::optional<Value>(builder.create<fhe::CastOp>(loc, destTy, vs));
+                }
+            }
+            else if (mlir::dyn_cast_or_null<fhe::PlainMatrixType>(srcTy)) {
+                if (destTy.getValue().str() == "std::vector<std::vector<Plain>>") {
+                    return std::optional<Value>(builder.create<fhe::CastOp>(loc, destTy, vs));
+                }
+            }
             else if (mlir::dyn_cast_or_null<fhe::IntType>(srcTy)) {
                 if (destTy.getValue().str() == "int") {
                     return std::optional<Value>(builder.create<fhe::CastOp>(loc, destTy, vs));
                 }
+            }
+            else {
+                LLVM_DEBUG(llvm::dbgs() << "No handling for the ValueRange type:" << srcTy <<"[at FheToEmitcPass materializeCommon].\n");
             }
         }
 
@@ -578,12 +608,39 @@ void LowerFheToEmitcPass::runOnOperation()
         else if (mlir::isa<fhe::PlainType>(t)) {
             return std::optional<Type>(emitc::OpaqueType::get(&getContext(), "Plain"));
         }
+        else if (mlir::isa<fhe::PlainVectorType>(t)) {
+            return std::optional<Type>(emitc::OpaqueType::get(&getContext(), "std::vector<Plain>"));
+        }
+        else if (mlir::isa<fhe::PlainMatrixType>(t)) {
+            return std::optional<Type>(emitc::OpaqueType::get(&getContext(), "std::vector<std::vector<Plain>>"));
+        }
         else if (mlir::isa<fhe::IntType>(t)) {
             return std::optional<Type>(emitc::OpaqueType::get(&getContext(), "int"));
         }
-        else {
-            return std::optional<Type>(t);
-        }
+        // deal with build-in type, the all following types mean clear types.
+        // else if (mlir::isa<MemRefType>(t)) {
+        //     auto newTy = mlir::cast<MemRefType>(t);
+        //     if (newTy.hasStaticShape() && newTy.getShape().size() == 0) {
+        //         return std::optional<Type>(emitc::OpaqueType::get(&getContext(), "std::vector<Plain>")); // rank:0
+        //     }
+        //     else if (newTy.hasStaticShape() && newTy.getShape().size() == 1) {
+        //         return std::optional<Type>(emitc::OpaqueType::get(&getContext(), "std::vector<Plain>"));
+        //     }
+        //     else if (newTy.hasStaticShape() && newTy.getShape().size() == 2) {
+        //         return std::optional<Type>(emitc::OpaqueType::get(&getContext(), "std::vector<std::vector<Plain>>"));
+        //     }
+        //     else {
+        //         llvm::errs() << "Unknow support rank is:" << newTy.getShape().size() << ".\n";
+        //         return std::optional<Type>(std::nullopt);
+        //     }
+        // }
+        // else if (mlir::isa<mlir::FloatType>(t) || mlir::isa<mlir::IntegerType>(t)) {
+        //     return std::optional<Type>(emitc::OpaqueType::get(&getContext(), "Plain"));
+        // }
+
+        LLVM_DEBUG(llvm::dbgs() << "Warning: No conver type:(" << t << ")[at FheToEmitcPass addConversion].\n");
+        return std::optional<Type>(t);
+    
     });
 
     type_converter.addTargetMaterialization([&](OpBuilder &builder, Type t, ValueRange vs, Location loc) {
@@ -698,10 +755,11 @@ void LowerFheToEmitcPass::runOnOperation()
             FheArithBinaryPattern<fhe::LWESubOp>, FheArithBinaryPattern<fhe::LWESubPlainOp>,
             FheArithBinaryPattern<fhe::LWEMulOp>, FheArithBinaryPattern<fhe::LWEMulPlainOp>, FheArithBinaryPattern<fhe::RLWEMulOp>,
             FheFuncPattern, FheRetPattern, FheCallPattern,
-            MemrefLoadPattern, FheLoadPattern, FheStorePattern,
+            NativeMemrefLoadPattern, FheLoadPattern, FheStorePattern,
             FheAllocaPattern, FheAllocPattern, FheDeallocPattern>(type_converter, fhePats.getContext());
 
     if (mlir::failed(mlir::applyPartialConversion(getOperation(), target, std::move(fhePats)))) {
+        getOperation()->emitError("FheToEmitc Partial conversion failed.");
         signalPassFailure();
     }
 }
