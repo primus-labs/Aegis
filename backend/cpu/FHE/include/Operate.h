2190f20b4f393f4fa963bb762fa2d06f1c56d582
cyf <yuanfeng@padolabs.org>
2025-04-14 12:14:41 +0800

    Added serialize and deserialize Ciphertext functions

--- backend/cpu/FHE/include/Operate.h
+++ backend/cpu/FHE/include/Operate.h
@@ -4,11 +4,16 @@
 #include <inttypes.h>
 #include <vector>
 #include <cstddef> // size_t
+#include <sstream>
+#include "openfhe.h"
+using namespace lbcrypto;
 
 namespace aegiscpu {
 
 std::vector<uint8_t> encrypt(const std::vector<double> &data);
 std::vector<double> decrypt(const std::vector<uint8_t> &data, size_t plaintextSize);
+void serializeCiphertext(const Ciphertext<DCRTPoly>& ciphertext, std::vector<uint8_t>& bytes);
+Ciphertext<DCRTPoly> deserializeCiphertext(const std::vector<uint8_t>& bytes);
 
 } // namespace aegiscpu
 
--- backend/cpu/FHE/src/Operate.cpp
+++ backend/cpu/FHE/src/Operate.cpp
@@ -5,41 +5,53 @@
 namespace aegiscpu {
 
 std::vector<uint8_t> encrypt(const std::vector<double> &data) {
-  CryptoContext<DCRTPoly> cc =
-      CryptoContextMgr::getInstance().getCryptoContext();
+    CryptoContext<DCRTPoly> cc = CryptoContextMgr::getInstance().getCryptoContext();
 
-  Plaintext ptValue = cc->MakeCKKSPackedPlaintext(data);
+    Plaintext ptValue = cc->MakeCKKSPackedPlaintext(data);
 
-  PublicKey<DCRTPoly> pk = FheKeyset::getInstance().getPubKey()->getKey();
-  Ciphertext<DCRTPoly> ctValue = cc->Encrypt(pk, ptValue);
+    PublicKey<DCRTPoly> pk = FheKeyset::getInstance().getPubKey()->getKey();
+    Ciphertext<DCRTPoly> ctValue = cc->Encrypt(pk, ptValue);
 
-  std::stringstream ss;
-  Serial::Serialize(ctValue, ss, SerType::BINARY);
-  std::vector<uint8_t> buffer((std::istreambuf_iterator<char>(ss)),
-                              std::istreambuf_iterator<char>());
+    std::stringstream ss;
+    Serial::Serialize(ctValue, ss, SerType::BINARY);
+    std::vector<uint8_t> buffer((std::istreambuf_iterator<char>(ss)), std::istreambuf_iterator<char>());
 
-  return buffer;
+    return buffer;
 }
 
 std::vector<double> decrypt(const std::vector<uint8_t> &data, size_t plaintextSize) {
-  CryptoContext<DCRTPoly> cc =
-      CryptoContextMgr::getInstance().getCryptoContext();
+    CryptoContext<DCRTPoly> cc = CryptoContextMgr::getInstance().getCryptoContext();
 
-  // Deserialize output byte array into a ciphertext object.
-  std::stringstream ss;
-  ss.write(reinterpret_cast<const char *>(data.data()), data.size());
-  Ciphertext<DCRTPoly> deserCiphertext;
-  Serial::Deserialize(deserCiphertext, ss, SerType::BINARY);
+    // Deserialize output byte array into a ciphertext object.
+    std::stringstream ss;
+    ss.write(reinterpret_cast<const char *>(data.data()), data.size());
+    Ciphertext<DCRTPoly> deserCiphertext;
+    Serial::Deserialize(deserCiphertext, ss, SerType::BINARY);
 
-  // get private key
-  PrivateKey<DCRTPoly> priKey = FheKeyset::getInstance().getPriKey()->getKey();
+    // get private key
+    PrivateKey<DCRTPoly> priKey = FheKeyset::getInstance().getPriKey()->getKey();
 
-  // decrypt
-  Plaintext ptValue;
-  cc->Decrypt(priKey, deserCiphertext, &ptValue);
-  ptValue->SetLength(plaintextSize); // Need know plaintext size for decrypting.
+    // decrypt
+    Plaintext ptValue;
+    cc->Decrypt(priKey, deserCiphertext, &ptValue);
+    ptValue->SetLength(plaintextSize); // Need know plaintext size for decrypting.
 
-  return ptValue->GetRealPackedValue();
+    return ptValue->GetRealPackedValue();
 }
 
+void serializeCiphertext(const Ciphertext<DCRTPoly>& ciphertext, std::vector<uint8_t>& bytes) {
+    std::stringstream ss;
+    Serial::Serialize(ciphertext, ss, SerType::BINARY);
+    std::string serialized = ss.str();
+    bytes.assign(serialized.begin(), serialized.end());
+}
+
+Ciphertext<DCRTPoly> deserializeCiphertext(const std::vector<uint8_t> &bytes) {
+    Ciphertext<DCRTPoly> newC;
+    std::string serialized(bytes.begin(), bytes.end());
+    std::stringstream ss(serialized);
+    Serial::Deserialize<Ciphertext<DCRTPoly>>(newC, ss, SerType::BINARY);
+    return newC;
+}
+    
 } // namespace aegiscpu
