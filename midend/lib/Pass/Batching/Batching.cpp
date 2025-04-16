7d1b26c6173da0a369a7d6a5b02a02dd4989d3a8
cyf <yuanfeng@padolabs.org>
2025-04-16 13:54:40 +0800

    Optimize Batching pass, not tested yet.

--- midend/lib/Pass/Batching/Batching.cpp
+++ midend/lib/Pass/Batching/Batching.cpp
@@ -7,6 +7,8 @@
 #include "mlir/Dialect/Utils/IndexingUtils.h"
 #include "mlir/Dialect/Utils/StaticValueUtils.h"
 #include "mlir/include/mlir/Support/LLVM.h"
+#include "mlir/Pass/PassManager.h"
+#include "mlir/Transforms/Passes.h"
 #include "llvm/ADT/APSInt.h"
 #include "llvm/include/llvm/Support/Debug.h"
 #include "Dialect/FHE/FHEDialect.h"
@@ -28,13 +30,13 @@ LogicalResult batchArithOperation(IRRewriter &rewriter, MLIRContext *context, Op
         // We only search one level deep for now.
         for (auto u : op->getUsers()) {
             if (fhe::LoadOp loadOp = mlir::dyn_cast_or_null<fhe::LoadOp>(u)) {
-                assert(static_cast<int64_t>(loadOp.getIndices().size()) == 1);
+                assert(static_cast<int64_t>(loadOp.getIndices().size()) == 1 && "LoadOp indices size > 1 not support");
                 auto idx = getConstantIntValue(loadOp.getIndices()[0]);
                 assert(idx.has_value());
                 target_slot = idx.value();
                 break;
             } else if (fhe::StoreOp storeOp = mlir::dyn_cast_or_null<fhe::StoreOp>(u)) {
-                assert(static_cast<int64_t>(storeOp.getIndices().size()) == 1);
+                assert(static_cast<int64_t>(storeOp.getIndices().size()) == 1 && "StoreOp indices size > 1 not support");
                 auto idx = getConstantIntValue(storeOp.getIndices()[0]);
                 assert(idx.has_value());
                 target_slot = idx.value();
@@ -54,7 +56,7 @@ LogicalResult batchArithOperation(IRRewriter &rewriter, MLIRContext *context, Op
                 if (mlir::isa<fhe::LWECipherType>((*it).getType())) {
                     // scalar-type input that needs to be converted
                     if (fhe::LoadOp loadOp = (*it).template getDefiningOp<fhe::LoadOp>()) {
-                        assert(static_cast<int64_t>(loadOp.getIndices().size()) == 1);
+                        assert(static_cast<int64_t>(loadOp.getIndices().size()) == 1 && "LoadOp indices size > 1 not support");
                         auto idx = getConstantIntValue(loadOp.getIndices()[0]);
                         assert(idx.has_value());
                         auto i = idx.value();
@@ -117,7 +119,7 @@ LogicalResult batchArithOperation(IRRewriter &rewriter, MLIRContext *context, Op
                     }
 
                     // Instead of using the load operation, use a rotation operation instead.
-                    assert(static_cast<int64_t>(loadOp.getIndices().size()) == 1);
+                    assert(static_cast<int64_t>(loadOp.getIndices().size()) == 1 && "LoadOp indices size > 1 not support");
                     auto idx = getConstantIntValue(loadOp.getIndices()[0]);
                     assert(idx.has_value());
                     auto i = idx.value();
@@ -158,6 +160,122 @@ LogicalResult batchArithOperation(IRRewriter &rewriter, MLIRContext *context, Op
         rewriter.eraseOp(op);
     }
 
+    return success();
+}
+
+template <typename OpType>
+LogicalResult batchLoadStoreOperation(IRRewriter &rewriter, MLIRContext *context, OpType op) {
+    rewriter.setInsertionPoint(op);
+    int target_slot = -1;
+
+    if (mlir::isa<fhe::LoadOp>(op)) {
+        /**********************************************************************************
+        %7 = fhe.load(%6, %c0) : (!fhe.lweciphervec<16 x f64>, index) -> !fhe.lwecipher<f64>
+        //fhe.store(%7, %arg1, %c0) : (!fhe.lwecipher<f64>, !fhe.lweciphervec<4 x f64>, index)
+        =>
+        %rotated = fhe.rotate(%6, 0) 
+        %mask = arith.constant dense<[1.0, 0.0, ..., 0.0]> : tensor<16xf64>
+        %7 = fhe.mulplain(%rotated, %mask) : !fhe.lweciphervec<16 x f64>
+        ************************************************************************************/
+        assert(static_cast<int64_t>(op.getIndices().size()) == 1 && "LoadOp indices size > 1 not support");
+        auto idx = getConstantIntValue(op.getIndices()[0]);
+        assert(idx.has_value());
+        target_slot = idx.value();
+
+        // find the maximum size of LoadOp memref operand
+        int max_size = -1;
+        for (auto operand : op.getOperands()) {
+            if (auto operandTy = mlir::dyn_cast_or_null<fhe::LWECipherVectorType>(operand.getType())) {
+                max_size = std::max(max_size, operandTy.getSize());
+            }
+        }
+
+        // Create rotation amount (negative index to bring element to position 0)
+        auto rotateAmount = -target_slot % max_size;
+
+         // Apply rotation operation
+        auto rotateOp = rewriter.create<fhe::RotateOp>(op.getLoc(), op.getMemref().getType(),
+                                                       op.getMemref(), rotateAmount);
+
+        // Create selection mask [1,0,0,...]
+        mlir::Type elementType = rewriter.getI32Type();
+        auto arrayType = mlir::VectorType::get({max_size}, elementType);
+        SmallVector<int32_t> maskValues(max_size, 0);
+        maskValues[0] = 1;
+        llvm::ArrayRef<int32_t> valuesRef(maskValues); 
+        mlir::DenseElementsAttr denseAttr = mlir::DenseElementsAttr::get(arrayType, valuesRef);
+        auto maskOp = rewriter.create<arith::ConstantOp>(op.getLoc(), denseAttr);
+
+        // Apply element-wise multiplication to select the first element
+        llvm::SmallVector<Value> operands;
+        operands.push_back(rotateOp);
+        operands.push_back(maskOp);
+        rewriter.replaceOpWithNewOp<fhe::LWEMulPlainOp>(op, rotateOp.getType(), operands);
+    } else if (mlir::isa<fhe::StoreOp>(op)) {
+        /***********************************************************************************
+        //%7 = fhe.load(%6, %c0) : (!fhe.lweciphervec<16 x f64>, index) -> !fhe.lwecipher<f64>
+        fhe.store(%7, %arg1, %c0) : (!fhe.lwecipher<f64>, !fhe.lweciphervec<4 x f64>, index)
+        =>
+        %inverted_mask = arith.constant dense<[0,1,1,1...]> : tensor<16xf64> 
+        %existing = fhe.mulplain %arg1, %inverted_mask : !fhe.lweciphervec<16xf64>
+        %new_val = fhe.add %existing, %7 : !fhe.lweciphervec<16xf64>
+        ***********************************************************************************/
+        auto storeOp = llvm::cast<fhe::StoreOp>(op);
+        assert(static_cast<int64_t>(storeOp.getIndices().size()) == 1 && "StoreOp indices size > 1 not support");
+        auto idx = getConstantIntValue(storeOp.getIndices()[0]);
+        assert(idx.has_value());
+        target_slot = idx.value();
+
+        // find the maximum size of StoreOp memref operand
+        int max_size = -1;
+        for (auto operand : storeOp.getOperands()) {
+            if (auto operandTy = mlir::dyn_cast_or_null<fhe::LWECipherVectorType>(operand.getType())) {
+                max_size = std::max(max_size, operandTy.getSize());
+            }
+        }
+
+        for (auto u : storeOp->getUsers()) {
+            if (fhe::LWEMulPlainOp mulplainOp = mlir::dyn_cast_or_null<fhe::LWEMulPlainOp>(u)) {
+                for (auto operand : mulplainOp.getOperands()) {
+                    if (auto constOp = operand.getDefiningOp<arith::ConstantOp>()) {
+                        Attribute valueAttr = constOp.getValue();
+                        if (auto denseAttr = mlir::dyn_cast<mlir::DenseElementsAttr>(valueAttr)) {
+                            // Convert to DenseElementsAttr(vector/tensor)
+                            auto constVals = denseAttr.getValues<int32_t>();
+                            size_t oneValIdx = 0;
+                            for (auto i = 0; i < constVals.size(); i++) {
+                                if (constVals[i] == 1) {
+                                    oneValIdx = i;
+                                    break;
+                                }
+                            }
+
+                            // Invert the mast value and create constOp
+                            mlir::Type elementType = rewriter.getI32Type();
+                            auto arrayType = mlir::VectorType::get({max_size}, elementType);
+                            SmallVector<int32_t> invertMask(max_size, 1);
+                            invertMask[oneValIdx] = 0;
+                            llvm::ArrayRef<int32_t> invertMaskRef(invertMask); 
+                            mlir::DenseElementsAttr denseAttr2 = mlir::DenseElementsAttr::get(arrayType, invertMaskRef);
+                            auto invertMaskOp = rewriter.create<arith::ConstantOp>(storeOp.getLoc(), denseAttr2);    
+
+                            // Create fhe.mulplain
+                            auto mulVal = rewriter.create<fhe::LWEMulPlainOp>(storeOp.getLoc(), storeOp.getMemref().getType(),
+                                                                              ValueRange({storeOp.getMemref(), invertMaskOp}));
+
+                            // Combine new and existing values
+                            auto result = rewriter.create<fhe::LWEAddOp>(storeOp.getLoc(), mulVal.getType(),
+                                                                         ValueRange({mulVal, storeOp.getValueToStore()}));
+
+                            // Replace the store operation with fheAddOp
+                            rewriter.replaceOp(op, result);
+                            break;               
+                        }
+                    }
+                }
+            }
+        }
+    }
     return success();
 }
 
@@ -258,10 +376,39 @@ void BatchingPass::runOnOperation() {
                     }
                 }
                 return WalkResult(success());
-            }).wasInterrupted())
-            signalPassFailure();
+        }).wasInterrupted())
+        signalPassFailure();
     }
 
+    // After the batch operation, exist some redundant load & store op, we mush to delete it.
+    // If there are redundant load & store operations, they can affect the subsequent ​​batchLoadStoreOperation​​ optimization.
+    PassManager nestedPM(&getContext());
+    nestedPM.addPass(createCanonicalizerPass());
+    nestedPM.addPass(createCSEPass());
+    if (failed(nestedPM.run(getOperation()))) {
+        signalPassFailure();
+    }
+
+
+    // After the batch operation is completed, load & store pairs may be reserved. At this time, 
+    // the load & store pairs need to be optimized and converted into rotate and corresponding arith operations.
+    for (auto funcOp : llvm::make_early_inc_range(block.getOps<func::FuncOp>())) {
+        if (funcOp.walk([&](Operation *op) {
+            if (auto loadOp = llvm::dyn_cast_or_null<fhe::LoadOp>(op)) {
+                    if (batchLoadStoreOperation<fhe::LoadOp>(rewriter, &getContext(), loadOp).failed()) {
+                        return WalkResult::interrupt();
+                    }
+            } else if (auto storeOp = llvm::dyn_cast_or_null<fhe::StoreOp>(op)) {
+                if (batchLoadStoreOperation<fhe::StoreOp>(rewriter, &getContext(), storeOp).failed()) {
+                    return WalkResult::interrupt();
+                }
+            }
+            return WalkResult(success());
+        }).wasInterrupted())
+        signalPassFailure();
+    }
+
+    // RotateOp type from LWE to RLWE
     auto RotOpToRLWEOp = [](IRRewriter &rewriter, MLIRContext *context, fhe::RotateOp op,
                             TypeConverter typeConverter) -> LogicalResult {
         rewriter.setInsertionPoint(op);
