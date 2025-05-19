321f7d2511cd1b882e5fb08aa013bd7b9c4ac9a5
cyf <yuanfeng@padolabs.org>
2025-05-19 19:08:46 +0800

    test(cppcheck): add test case

---
+++ midend/tests/cppcheck/compare.cpp
@@ -0,0 +1,313 @@
+// RUN: test_cpp_to_bin.sh  %s | FileCheck %s
+
+#include "openfhe.h"
+#include <vector>
+#include <iostream>
+#include <cmath>
+#include <functional>
+
+using namespace lbcrypto;
+
+
+#include <vector>
+#include <iostream>
+#include "openfhe.h"
+using namespace std;
+using namespace lbcrypto;
+using CiphertextT = Ciphertext<DCRTPoly>;
+using RLWECipher = Ciphertext<DCRTPoly>;
+using LWECipher = Ciphertext<DCRTPoly>;
+using PlaintextT = Plaintext;
+using Plain = double;
+using PlainVector = std::vector<double>;
+using PlainMatrix = std::vector<PlainVector>;
+using MutableCiphertextT = Ciphertext<DCRTPoly>;
+using CCParamsT = CCParams<CryptoContextCKKSRNS>;
+using CryptoContextT = CryptoContext<DCRTPoly>;
+using EvalKeyT = EvalKey<DCRTPoly>;
+using PrivateKeyT = PrivateKey<DCRTPoly>;
+using PublicKeyT = PublicKey<DCRTPoly>;
+#define Copy(src, dest) dest = src
+#define Add(a, b) clientCC->EvalAdd((a), (b))
+#define AddPlain(c, p) AddPlainImpl((c), (p))
+#define Sub(a, b) clientCC->EvalSub((a), (b))
+#define SubPlain(c, p) SubPlainImpl((c), (p))
+#define Mul(a, b) clientCC->EvalMult((a), (b))
+#define MulPlain(c, p) MulPlainImpl((c), (p))
+#define Rotate(c, idx) clientCC->EvalRotate((c), (idx))
+#define Bootstrap(a) clientCC->EvalBootstrap(a)
+#define MakePlain(a)  double(a)
+#define MakeMultPlain(...) std::vector<double>{__VA_ARGS__}
+#define Alloc() RLWECipher()    //added
+#define Cast_Plain_To_Index(clr) size_t(clr)
+#define LoadPlainWithIndex(v, idx) v[idx]
+#define LoadPlainWithoutIndex(v) v[0]
+#define Cast_Stub(a) a        
+#define ConstantArray_to_PlainVector(ary) std::vector<double>(ary, ary + std::size(ary)) 
+
+static CryptoContext<DCRTPoly> clientCC;
+static PublicKey<DCRTPoly> clientPubKey;
+static std::string ccFileName, pubKeyFileName, mulKeyFileName, rotKeyFileName;
+
+extern "C"
+bool loadCryptoResources(const std::string &ccLoc,  const std::string &pubKeyLoc, 
+                         const std::string &multKeyLoc, const std::string &rotKeyLoc) {
+    ccFileName = ccLoc;
+    pubKeyFileName = pubKeyLoc;
+    mulKeyFileName = multKeyLoc;
+    rotKeyFileName = rotKeyLoc;
+    return true;
+}
+
+
+void initCryptContext() {
+    int ccSizes = CryptoContextFactory<DCRTPoly>::GetContextCount();
+    std::cout << "before call GenCryptoContext, crypto context obj counts:" << ccSizes << std::endl;
+    if (ccSizes > 0) {
+        // use exist crypto context obj
+        std::vector<uint32_t> levelBudget = {3, 1};
+        unsigned mulDepth = 8;
+        if (1) {
+            SecretKeyDist secretKeyDist = UNIFORM_TERNARY;
+            mulDepth += FHECKKSRNS::GetBootstrapDepth(levelBudget, secretKeyDist);
+        }
+        CCParams<CryptoContextCKKSRNS> parameters;
+        parameters.SetMultiplicativeDepth(mulDepth);
+        parameters.SetFirstModSize(60);
+        parameters.SetScalingModSize(50);
+        parameters.SetBatchSize(1);
+        clientCC = GenCryptoContext(parameters);
+        ccSizes = CryptoContextFactory<DCRTPoly>::GetContextCount();
+        std::cout << "after call GenCryptoContext, crypto context obj counts:" << ccSizes << std::endl;
+    } else {
+        clientCC->ClearEvalMultKeys();
+        clientCC->ClearEvalAutomorphismKeys();
+        lbcrypto::CryptoContextFactory<lbcrypto::DCRTPoly>::ReleaseAllContexts();
+        if (!Serial::DeserializeFromFile(ccFileName, clientCC, SerType::BINARY)) {
+            std::cerr << "Cannot read serialized data from: " << ccFileName << std::endl;
+            std::exit(1);
+        }
+
+        if (!Serial::DeserializeFromFile(pubKeyFileName, clientPubKey, SerType::BINARY)) {
+            std::cerr << "Cannot read serialized data from: " << pubKeyFileName << std::endl;
+            std::exit(1);
+        }
+
+        std::ifstream multKeyIStream(mulKeyFileName, std::ios::in | std::ios::binary);
+        if (!multKeyIStream.is_open()) {
+            std::cerr << "Cannot read serialization from " << mulKeyFileName << std::endl;
+            std::exit(1);
+        }
+        if (!clientCC->DeserializeEvalMultKey(multKeyIStream, SerType::BINARY)) {
+            std::cerr << "Could not deserialize eval mult key file" << std::endl;
+            std::exit(1);
+        }
+
+        if (!rotKeyFileName.empty()) {
+            std::ifstream rotKeyIStream(rotKeyFileName, std::ios::in | std::ios::binary);
+            if (!rotKeyIStream.is_open()) {
+                std::cerr << "Cannot read serialization from " << rotKeyFileName << std::endl;
+                std::exit(1);
+            }
+            if (!clientCC->DeserializeEvalAutomorphismKey(rotKeyIStream, SerType::BINARY)) {
+                std::cerr << "Could not deserialize eval rot key file" << std::endl;
+                std::exit(1);
+            }
+        }
+    }
+}
+
+inline RLWECipher AddPlainImpl(RLWECipher a, Plain b) {
+    return clientCC->EvalAdd(a, b);
+}
+inline RLWECipher AddPlainImpl(RLWECipher a, PlainVector b) {
+    return clientCC->EvalAdd(a, clientCC->MakeCKKSPackedPlaintext(b));
+}
+inline RLWECipher SubPlainImpl(RLWECipher a, Plain b) {
+    return clientCC->EvalSub(a, b);
+}
+inline RLWECipher SubPlainImpl(RLWECipher a, PlainVector b) {
+    return clientCC->EvalSub(a, clientCC->MakeCKKSPackedPlaintext(b));
+}
+inline RLWECipher MulPlainImpl(RLWECipher a, Plain b) {
+    return clientCC->EvalMult(a, b);
+}
+inline RLWECipher MulPlainImpl(RLWECipher a, PlainVector b) {
+    return clientCC->EvalMult(a, clientCC->MakeCKKSPackedPlaintext(b));
+}
+
+
+// [-10,0.01] ∪ [0.01,10] deg_list = [12, 20, 20]  epsilon = 0.01
+std::vector<double> coeff1 = {1.83684403e+00, -2.70230296e-01, 1.39933082e-02, -3.08382753e-04, 3.01902138e-06, -1.08264445e-08};
+std::vector<double> coeff2 = {1.36528002e+01, -1.44508325e+02, 5.88075783e+02, -1.14488119e+03, 1.23031707e+03, -7.83728468e+02, 3.03280966e+02, -6.99653527e+01, 8.84524133e+00, -4.71519103e-01};
+std::vector<double> coeff3 = {5.46535806e+00, -3.13317662e+01, 1.10634375e+02, -2.21351538e+02, 2.64761671e+02, -1.96093273e+02, 9.05786026e+01, -2.53712497e+01, 3.94000641e+00, -2.60142549e-01};
+
+template <typename T>
+inline uint64_t ceilLog2(T x) {
+    return static_cast<uint64_t>(std::ceil(std::log2(x)));
+}
+
+void EvalPower(std::vector<double> coefficients, std::vector<Ciphertext<lbcrypto::DCRTPoly>>& power_basis,
+               Ciphertext<lbcrypto::DCRTPoly>& result) {
+    if (coefficients.size() == 1) {
+        if (std::fabs(std::round(coefficients[0] * pow(2, 50))) > 1.) {
+            result = clientCC->EvalMult(power_basis[0], coefficients[0]);
+            return;
+        }
+        else {
+            return;
+        }
+    }
+
+    std::vector<double> quotient, remainder;
+    uint64_t degree = 2 * coefficients.size() - 1;
+    uint64_t m      = ceilLog2(degree + 1);
+    remainder.resize((1 << (m - 1)) / 2);
+    quotient.resize(coefficients.size() - remainder.size());
+
+    for (size_t i = 0; i < remainder.size(); i++) {
+        remainder[i] = coefficients[i];
+    }
+    for (size_t i = 0; i < quotient.size(); i++) {
+        quotient[i] = coefficients[i + remainder.size()];
+    }
+
+    Ciphertext<lbcrypto::DCRTPoly> cipher_quotient, cipher_remainder;
+    EvalPower(quotient, power_basis, cipher_quotient);
+    EvalPower(remainder, power_basis, cipher_remainder);
+    result = clientCC->EvalMult(cipher_quotient, power_basis[m - 1]);
+    result = clientCC->EvalAdd(result, cipher_remainder);
+}
+
+
+void polyEvalPower(Ciphertext<DCRTPoly>& result, Ciphertext<DCRTPoly>& x, std::vector<double>& coefficients) {
+    uint64_t degree = coefficients.size() * 2 - 1;
+    uint64_t m      = ceilLog2(degree + 1);
+
+    std::vector<Ciphertext<DCRTPoly>> power_basis(m);
+    power_basis[0] = x;
+    for (size_t i = 1; i < m; i++) {
+        power_basis[i] = clientCC->EvalMult(power_basis[i - 1], power_basis[i - 1]);
+    }
+
+    EvalPower(coefficients, power_basis, result);
+}
+
+
+Ciphertext<DCRTPoly> EvalCmpWithZero(Ciphertext<DCRTPoly>& x) {
+    Ciphertext<DCRTPoly> result, x1, x2;
+    polyEvalPower(x1, x, coeff1);
+    polyEvalPower(x2, x1, coeff2);
+    for (auto &x : coeff3) {
+        x /= 2.0;
+    }
+    polyEvalPower(result, x2, coeff3);
+    result = clientCC->EvalAdd(result, 0.5);
+    return result;
+}
+
+// c * (2c - 1) => 2c^2 - c
+inline Ciphertext<DCRTPoly> EvalCmp_gt(const Ciphertext<DCRTPoly> &x, const Ciphertext<DCRTPoly> &y) {
+    auto z = clientCC->EvalSub(x, y);
+    auto cond = EvalCmpWithZero(z);
+    Plaintext plainTwo = clientCC->MakeCKKSPackedPlaintext(std::vector(1, 2.0));
+    auto cipherTwo     = clientCC->Encrypt(clientPubKey, plainTwo);
+    return clientCC->EvalSub(clientCC->EvalMult(clientCC->EvalMult(cond, cond), cipherTwo), cond);
+}
+
+//4c(1 - c) => 4c - 4c^2
+inline Ciphertext<DCRTPoly> EvalCmp_eq(const Ciphertext<DCRTPoly> &x, const Ciphertext<DCRTPoly> &y) {
+    auto z = clientCC->EvalSub(x, y);
+    auto cond = EvalCmpWithZero(z);
+    Plaintext plainFour = clientCC->MakeCKKSPackedPlaintext(std::vector(1, 4.0));
+    auto cipherFour     = clientCC->Encrypt(clientPubKey, plainFour);
+    auto FourCond = clientCC->EvalMult(cipherFour, cond);
+    return clientCC->EvalSub(FourCond, clientCC->EvalMult(FourCond, cond));
+}
+
+// 1 - is_gt - is_equal => 1 - (2c^2 - c) - (4c - 4c^2) => 1 - 3c + 2c^2
+inline Ciphertext<DCRTPoly> EvalCmp_lt(const Ciphertext<DCRTPoly> &x, const Ciphertext<DCRTPoly> &y) {
+    auto z = clientCC->EvalSub(x, y);
+    auto cond = EvalCmpWithZero(z);
+    Plaintext plainOne   = clientCC->MakeCKKSPackedPlaintext(std::vector(1, 1.0));
+    Plaintext plainTwo   = clientCC->MakeCKKSPackedPlaintext(std::vector(1, 2.0));
+    Plaintext plainThree = clientCC->MakeCKKSPackedPlaintext(std::vector(1, 3.0));
+    auto cipherOne       = clientCC->Encrypt(clientPubKey, plainOne);
+    auto cipherTwo       = clientCC->Encrypt(clientPubKey, plainTwo);
+    auto cipherThree     = clientCC->Encrypt(clientPubKey, plainThree);
+    auto cipherThreeCond = clientCC->EvalMult(cipherThree, cond);
+    auto cipherTwoCondPower = clientCC->EvalMult(cipherTwo, clientCC->EvalMult(cond, cond));
+    return clientCC->EvalAdd(clientCC->EvalSub(cipherOne, cipherThreeCond), cipherTwoCondPower);
+}
+
+// is_great + is_equal => c * (2c - 1) + 4c(1 - c) => 2c^2 - c + 4c - 4c^2 => 3c - 2c^2
+inline Ciphertext<DCRTPoly> EvalCmp_ge(const Ciphertext<DCRTPoly> &x, const Ciphertext<DCRTPoly> &y) { 
+    auto z = clientCC->EvalSub(x, y);
+    auto cond = EvalCmpWithZero(z);
+    Plaintext plainTwo   = clientCC->MakeCKKSPackedPlaintext(std::vector(1, 2.0));
+    Plaintext plainThree = clientCC->MakeCKKSPackedPlaintext(std::vector(1, 3.0));
+    auto cipherTwo       = clientCC->Encrypt(clientPubKey, plainTwo);
+    auto cipherThree     = clientCC->Encrypt(clientPubKey, plainThree);
+    auto cipherThreeCond = clientCC->EvalMult(cipherThree, cond);
+    auto cipherTwoCondPower = clientCC->EvalMult(cipherTwo, clientCC->EvalMult(cond, cond));
+    return clientCC->EvalSub(cipherThreeCond, cipherTwoCondPower);
+}
+
+// is_less + is_equal => (1 - 3c + 2c^2) + (4c - 4c^2) => 1 + c - 2c^2
+inline Ciphertext<DCRTPoly> EvalCmp_le(const Ciphertext<DCRTPoly> &x, const Ciphertext<DCRTPoly> &y) {
+    auto z = clientCC->EvalSub(x, y);
+    auto cond = EvalCmpWithZero(z);
+    Plaintext plainOne   = clientCC->MakeCKKSPackedPlaintext(std::vector(1, 1.0));
+    Plaintext plainTwo   = clientCC->MakeCKKSPackedPlaintext(std::vector(1, 2.0));
+    auto cipherOne       = clientCC->Encrypt(clientPubKey, plainOne);
+    auto cipherTwo       = clientCC->Encrypt(clientPubKey, plainTwo);
+    auto cipherTwoCondPower = clientCC->EvalMult(cipherTwo, clientCC->EvalMult(cond, cond));
+    return clientCC->EvalSub(clientCC->EvalAdd(cipherOne, cond), cipherTwoCondPower);
+}
+
+// 1 - is_equal => 1 - (4c - 4c^2)
+inline Ciphertext<DCRTPoly> EvalCmp_ue(const Ciphertext<DCRTPoly> &x, const Ciphertext<DCRTPoly> &y) {
+    Plaintext plainOne   = clientCC->MakeCKKSPackedPlaintext(std::vector(1, 1.0));
+    auto cipherOne       = clientCC->Encrypt(clientPubKey, plainOne);
+    return clientCC->EvalSub(cipherOne, EvalCmp_eq(x, y));
+}
+
+
+inline Ciphertext<DCRTPoly> EvalSel(const Ciphertext<DCRTPoly> &cond, const Ciphertext<DCRTPoly> &x, 
+                                    const Ciphertext<DCRTPoly> &y) {
+    // result = cond * x + (1-cond) * y
+    Plaintext plainOne = clientCC->MakeCKKSPackedPlaintext(std::vector(1, 1.0));
+    auto cipherOne     = clientCC->Encrypt(clientPubKey, plainOne);
+    auto notCond       = clientCC->EvalSub(cipherOne, cond);
+    return clientCC->EvalAdd(clientCC->EvalMult(cond, x), clientCC->EvalMult(notCond, y));
+}
+
+
+RLWECipher main_graph(RLWECipher v1, RLWECipher v2) {
+    auto cond = EvalCmp_gt(v1, v2);
+    return EvalSel(cond, v1, v2);
+}
+
+extern "C" 
+std::vector<uint8_t> aegis_mlir_MVP(const std::vector<uint8_t> &buf1, const std::vector<uint8_t> &buf2) {
+    initCryptContext();
+
+    Ciphertext<DCRTPoly> v1;
+    std::stringstream ss1;
+    ss1.write(reinterpret_cast<const char *>(buf1.data()), buf1.size());
+    Serial::Deserialize(v1, ss1, SerType::BINARY);
+
+    Ciphertext<DCRTPoly> v2;
+    std::stringstream ss2;
+    ss2.write(reinterpret_cast<const char *>(buf2.data()), buf2.size());
+    Serial::Deserialize(v2, ss2, SerType::BINARY);
+
+    RLWECipher retV = main_graph(v1, v2);
+    std::stringstream retss;
+    Serial::Serialize(retV, retss, SerType::BINARY);
+    std::vector<uint8_t> retBuf((std::istreambuf_iterator<char>(retss)), std::istreambuf_iterator<char>());
+    return retBuf;
+}
+
+
+//CHECK: Pass
