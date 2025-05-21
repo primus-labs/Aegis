678299a1e3a41d152c6b2e278b29c696f70c9dd0
ujnss <unlinunlin@163.com>
2025-05-21 03:05:24 +0000

    feat(scripts): add build wheel script for frontend

---
+++ scripts/build_wheel.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+cd "$SCRIPT_DIR"
+
+#
+# Build the project
+# cd ${SCRIPT_DIR}
+# bash ./build.sh
+
+#
+# Copy executable and shared library files
+cd ${SCRIPT_DIR}
+bash ./copy_files_to_frontend.sh
+
+#
+# Build wheel
+# pip install build
+# apt install python3.10-venv
+FRONTEND_DIR=${SCRIPT_DIR}/../frontend
+cd ${FRONTEND_DIR}
+
+python -m build
+
+#
+# Install wheel
+# pip install dist/primus*.whl
