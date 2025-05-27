6db351f4d4f0a73b7c9b451c473893d040d44660
cyf <yuanfeng@padolabs.org>
2025-05-27 13:49:47 +0800

    feat(LowerMultiDimLoadPass): implement LowerMultiDimLoad pass(not yet test)

--- midend/lib/Pass/MultiDimLoad/LowerMultiDimLoad.cpp
+++ midend/lib/Pass/MultiDimLoad/LowerMultiDimLoad.cpp
@@ -17,10 +17,76 @@
 using namespace mlir;
 using namespace aegis;
 
+struct LowerMultiDimLoadPattern : public OpRewritePattern<fhe::LoadOp> {
+    LowerMultiDimLoadPattern(MLIRContext *context) : OpRewritePattern<fhe::LoadOp>(context, /*benefit=*/1) {}
+
+    LogicalResult matchAndRewrite(fhe::LoadOp loadOp, PatternRewriter &rewriter) const override {
+        // Check if it's multi-dimensional
+        auto indices = loadOp.getIndices();
+        if (indices.size() <= 1) {
+            // Skip 1D load
+            return failure();
+        }
+        if (indices.size() > 2) {
+            return loadOp.emitError("Higher than two-dimensional loads are not supported.");
+        }
+
+        // Verify memref type
+        auto memRefTy = mlir::dyn_cast<fhe::LWECipherMatrixType>(loadOp.getMemref().getType());
+        if (!memRefTy) {
+            return loadOp.emitError("Unsupported memory type ") << memRefTy;
+        }
+
+        // Get row & col form the memref type
+        auto row = getConstantIntValue(indices[0]);
+        auto col = getConstantIntValue(indices[1]);
+        assert(row.has_value() && col.has_value());
+        auto nRow = row.value();
+        auto nCol = col.value();
+
+        // Create reduced-dimension type
+        mlir::Value rowVal = rewriter.create<arith::ConstantOp>(loadOp.getLoc(), rewriter.getIndexAttr(nRow));
+        mlir::Value colVal = rewriter.create<arith::ConstantOp>(loadOp.getLoc(), rewriter.getIndexAttr(nCol));
+        auto newMemRefType = fhe::LWECipherVectorType::get(rewriter.getContext(), memRefTy.getPlaintextType(), nCol);
+
+        // Create vload operation
+        auto vloadOp = rewriter.create<fhe::VloadOp>(
+            loadOp.getLoc(), 
+            newMemRefType,          // Reduced-dimension type
+            loadOp.getMemref(),     // Original memref
+            rowVal                  // Row index
+        );
+
+        // Create new load operation
+        auto newLoadOp = rewriter.create<fhe::LoadOp>(
+            loadOp.getLoc(), 
+            loadOp.getType(),       // Keep original result type
+            vloadOp,                // Result from vload
+            colVal                  // col index
+        );
+
+        // Replace original operation
+        rewriter.replaceOp(loadOp, newLoadOp);
+        return success();
+    }
+};
+
 void LowerMultiDimLoadPass::getDependentDialects(mlir::DialectRegistry &registry) const {
     registry.insert<fhe::FHEDialect, func::FuncDialect>();
 }
 
 void LowerMultiDimLoadPass::runOnOperation() {
-    //TODO
+    MLIRContext *ctx = &getContext();
+
+    // Configure pattern set
+    RewritePatternSet patterns(ctx);
+    patterns.add<LowerMultiDimLoadPattern>(ctx);
+
+    // Configure greedy rewrite strategy
+    GreedyRewriteConfig config;
+    config.useTopDownTraversal = true; // top-down traversal
+
+    if (failed(applyPatternsAndFoldGreedily(getOperation(), std::move(patterns), config))) {
+        signalPassFailure();
+    }
 }
