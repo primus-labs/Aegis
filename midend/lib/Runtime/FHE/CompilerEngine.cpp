4c0035b575e7372ec319ccc7f6f827336d389897
cyf <yuanfeng@padolabs.org>
2025-05-29 19:12:08 +0800

    fix(CompilerEngine): generate prog_spec.json before lowerEmitcFinalize

--- midend/lib/Runtime/FHE/CompilerEngine.cpp
+++ midend/lib/Runtime/FHE/CompilerEngine.cpp
@@ -133,12 +133,14 @@ llvm::Expected<CompileResult> CompilerEngine::compile(mlir::ModuleOp module) {
     }
 
     // Generate prog_spec.json
-    if (options.target == TARGET::CPP || options.target == TARGET::LIBRARY) {
+    if (options.target == TARGET::EMITC || options.target == TARGET::CPP || 
+        options.target == TARGET::LIBRARY) {
         auto progSpecOrErr = createProgramSpec(module, options);
         if (!progSpecOrErr) {
             return progSpecOrErr.takeError();
         }
 
+        llvm::sys::fs::remove(fullProgSpecJsonFileName);
         if (!emitProgragSpecToJson(fullProgSpecJsonFileName, progSpecOrErr.get())) {
             return ErrorMsg("Failed to generate program spec json file.");
         }
