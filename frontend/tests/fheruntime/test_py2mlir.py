0da8674f874e5538323acc81179b7aadb9bb4861
xiaojiqing <xiaojiqing@padolabs.org>
2025-05-29 06:36:21 +0000

    test(py2mlir): add arith and comparison test cases

---
+++ frontend/tests/fheruntime/test_py2mlir.py
@@ -0,0 +1,49 @@
+from primus.aegis.fheruntime import Py2MLIRConverter
+
+def test_case_add(x1: float, x2: float):
+    return x1 + x2
+
+def test_case_sub(x1: float, x2: float):
+    return x1 - x2
+
+def test_case_mul(x1: float, x2: float):
+    return x1 * x2
+
+def test_case_div(x1: float, x2: float):
+    return x1 / x2
+
+def test_case_mod(x1: int, x2: int):
+    return x1 % x2
+
+def test_case_gt(x1: float, x2: float):
+    return x1 > x2
+
+def test_case_lt(x1: float, x2: float):
+    return x1 < x2
+
+def test_case_ge(x1: float, x2: float):
+    return x1 >= x2
+
+def test_case_le(x1: float, x2: float):
+    return x1 <= x2
+
+def test_case_eq(x1: float, x2: float):
+    return x1 == x2
+
+def test_case_neq(x1: float, x2: float):
+    return x1 != x2
+
+if __name__ == '__main__':
+    converter = Py2MLIRConverter('./test_cases')
+    converter.convert(test_case_add)
+    converter.convert(test_case_sub)
+    converter.convert(test_case_mul)
+    converter.convert(test_case_div)
+    converter.convert(test_case_mod)
+
+    converter.convert(test_case_gt)
+    converter.convert(test_case_lt)
+    converter.convert(test_case_ge)
+    converter.convert(test_case_le)
+    converter.convert(test_case_eq)
+    converter.convert(test_case_neq)
