db3e5235aadb0e3da93d8ce2ab5b49d2a78017da
cyf <yuanfeng@padolabs.org>
2025-06-03 17:09:59 +0800

    feat(cpubackend): add batch decryption api

--- backend/cpu/FHE/include/Operate.h
+++ backend/cpu/FHE/include/Operate.h
@@ -16,6 +16,11 @@ std::vector<double> decrypt(const std::vector<uint8_t> &data, size_t plaintextSi
 void serializeCiphertext(const Ciphertext<DCRTPoly>& ciphertext, std::vector<uint8_t>& bytes);
 Ciphertext<DCRTPoly> deserializeCiphertext(const std::vector<uint8_t>& bytes);
 
+// ecrypt multiple ciphertexts in a contiguous buffer.
+// The buffer data is divided into n equal parts. The size of data must be divisible by n.
+// Each part is then decrypted to a plaintext vector of size `plaintextSize`.
+std::vector<std::vector<double>> decryptBatch(const std::vector<uint8_t> &data, size_t n, size_t plaintextSize);
+
 } // namespace openfhe
 } // namespace aegiscpu
 
--- backend/cpu/FHE/src/Operate.cpp
+++ backend/cpu/FHE/src/Operate.cpp
@@ -1,6 +1,7 @@
 #include "Operate.h"
 #include "CryptoContextMgr.h"
 #include "FheKeyset.h"
+#include <cassert>
 
 namespace aegiscpu {
 namespace openfhe {
@@ -38,6 +39,47 @@ std::vector<double> decrypt(const std::vector<uint8_t> &data, size_t plaintextSi
     ptValue->SetLength(plaintextSize); // Need know plaintext size for decrypting.
 
     return ptValue->GetRealPackedValue();
+}
+
+std::vector<std::vector<double>> decryptBatch(const std::vector<uint8_t> &data, size_t n, size_t plaintextSize) {
+    CryptoContext<DCRTPoly> cc = CryptoContextMgr::getInstance().getCryptoContext();
+
+    std::stringstream ss;
+    ss.write(reinterpret_cast<const char *>(data.data()), data.size());
+
+    // get private key
+    PrivateKey<DCRTPoly> priKey = FheKeyset::getInstance().getPriKey()->getKey();
+
+    // Loop through each serialized ciphertext and decrypt
+    std::vector<Plaintext> decryptedTexts;
+    while (ss && ss.peek() != EOF) {
+        try {
+            // Deserialize single ciphertext
+            Ciphertext<DCRTPoly> ct;
+            Serial::Deserialize(ct, ss, SerType::BINARY);
+            
+            // Decrypt ciphertext
+            Plaintext pt;
+            cc->Decrypt(priKey, ct, &pt);
+            decryptedTexts.push_back(pt);
+        }
+        catch (const std::exception& e) {
+            std::cerr << "decryptBatch error: " << e.what() << std::endl;
+            break;
+        }
+    }
+    assert(decryptedTexts.size() == n);
+
+    // get plaintext values
+    std::vector<std::vector<double>> result;
+    for (auto i = 0; i < decryptedTexts.size(); i++) {
+        Plaintext ptValue = decryptedTexts[i];
+        ptValue->SetLength(plaintextSize); 
+        auto realVal = ptValue->GetRealPackedValue();
+        result.emplace_back(realVal);
+    }
+
+    return result;
 }
 
 void serializeCiphertext(const Ciphertext<DCRTPoly>& ciphertext, std::vector<uint8_t>& bytes) {
