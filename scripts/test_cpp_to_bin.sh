2c215247ba4ba59016467a59a1f276218512e4f5
cyf <yuanfeng@padolabs.org>
2025-06-10 09:32:52 +0800

    feat(build): add verbose logging for external dependency downloads

--- scripts/test_cpp_to_bin.sh
+++ scripts/test_cpp_to_bin.sh
@@ -50,7 +50,7 @@ $COMPILER -g -shared -fPIC -std=c++17 -O2 \
 # Check build status and display results
 if [ $? -eq 0 ]; then
     echo "\033[1;34mPass\033[0m"
-    rm libtest.so
+    #rm libtest.so
     #file libtest.so | grep -E 'shared object|ELF'
     #echo -e "\nLibrary dependencies:"
     #ldd libtest.so | grep OpenFHE
