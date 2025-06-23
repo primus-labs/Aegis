e7e0144f20ddc8e9190f84c4ed7bbe09d2d13021
ujnss <unlinunlin@163.com>
2025-06-23 05:48:46 +0000

    chore(frontend): add more apply_mlir case

--- frontend/primus/aegis/fheruntime/mlir_tool.py
+++ frontend/primus/aegis/fheruntime/mlir_tool.py
@@ -137,6 +137,53 @@ module attributes {llvm.data_layout = "e-m:e-p270:32:32-p271:32:32-p272:64:64-i6
 """
     assert output_mlir == expected_output_mlir
 
+    input_mlir = """module attributes {llvm.data_layout = "e-m:e-p270:32:32-p271:32:32-p272:64:64-i64:64-i128:128-f80:128-n8:16:32:64-S128", llvm.target_triple = "x86_64-unknown-linux-gnu", "onnx-mlir.symbol-postfix" = "muladd_model"} {
+  func.func @main_graph(%arg0: memref<8xf32> {onnx.name = "input_x"}, %arg1: memref<1xf32> {onnx.name = "input_y"}) -> (memref<8xf32> {onnx.name = "output"}) attributes {llvm.emit_c_interface} {
+    %c0 = arith.constant 0 : index
+    %0 = "krnl.global"() {name = "constant_0", shape = [], value = dense<2.000000e+00> : tensor<f32>} : () -> memref<f32>
+    %alloc = memref.alloc() {alignment = 16 : i64} : memref<1xf32>
+    %1 = affine.load %arg1[%c0] : memref<1xf32>
+    %2 = affine.load %0[] : memref<f32>
+    %3 = arith.mulf %1, %2 : f32
+    affine.store %3, %alloc[%c0] : memref<1xf32>
+    %alloc_0 = memref.alloc() {alignment = 16 : i64} : memref<8xf32>
+    affine.for %arg2 = 0 to 8 {
+      %4 = affine.load %arg0[%arg2] : memref<8xf32>
+      %5 = affine.load %alloc[%c0] : memref<1xf32>
+      %6 = arith.addf %4, %5 : f32
+      affine.store %6, %alloc_0[%arg2] : memref<8xf32>
+    }
+    return %alloc_0 : memref<8xf32>
+  }
+  "krnl.entry_point"() {func = @main_graph, numInputs = 2 : i32, numOutputs = 1 : i32, signature = "[    { \22type\22 : \22f32\22 , \22dims\22 : [8] , \22name\22 : \22input_x\22 }\0A ,    { \22type\22 : \22f32\22 , \22dims\22 : [1] , \22name\22 : \22input_y\22 }\0A\0A]\00@[   { \22type\22 : \22f32\22 , \22dims\22 : [8] , \22name\22 : \22output\22 }\0A\0A]\00"} : () -> ()
+}
+"""
+    output_mlir = apply_mlir(input_mlir)
 
+    expected_output_mlir = """module attributes {llvm.data_layout = "e-m:e-p270:32:32-p271:32:32-p272:64:64-i64:64-i128:128-f80:128-n8:16:32:64-S128", llvm.target_triple = "x86_64-unknown-linux-gnu", "onnx-mlir.symbol-postfix" = "muladd_model"} {
+  memref.global @constant_0 : memref<f32> = dense<2.000000e+00>
+  func.func @main_graph(%arg0: memref<8xf32> {onnx.name = "input_x"}, %arg1: memref<1xf32> {onnx.name = "input_y"}) -> (memref<8xf32> {onnx.name = "output"}) attributes {llvm.emit_c_interface} {
+    %c0 = arith.constant 0 : index
+    %0 = memref.get_global @constant_0 : memref<f32>
+    %alloc = memref.alloc() {alignment = 16 : i64} : memref<1xf32>
+    %1 = affine.load %arg1[%c0] : memref<1xf32>
+    %2 = affine.load %0[] : memref<f32>
+    %3 = arith.mulf %1, %2 : f32
+    affine.store %3, %alloc[%c0] : memref<1xf32>
+    %alloc_0 = memref.alloc() {alignment = 16 : i64} : memref<8xf32>
+    affine.for %arg2 = 0 to 8 {
+      %4 = affine.load %arg0[%arg2] : memref<8xf32>
+      %5 = affine.load %alloc[%c0] : memref<1xf32>
+      %6 = arith.addf %4, %5 : f32
+      affine.store %6, %alloc_0[%arg2] : memref<8xf32>
+    }
+    return %alloc_0 : memref<8xf32>
+  }
+}
+"""
+    assert output_mlir == expected_output_mlir
+
+
+# python ./frontend/primus/aegis/fheruntime/mlir_tool.py
 if __name__ == "__main__":
     _test_apply_mlir()
