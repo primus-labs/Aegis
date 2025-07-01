1ff1e55b51e00cd3ad6a8b8e5ea0efb67ebb1951
cyf <yuanfeng@padolabs.org>
2025-07-01 09:51:10 +0800

    chore(script): update script for SimRuntime

--- scripts/copy_files_to_frontend.sh
+++ scripts/copy_files_to_frontend.sh
@@ -8,10 +8,16 @@ cd "$SCRIPT_DIR"
 #
 primus_aegis_so=$(ls ${SCRIPT_DIR}/../build/lib/Binding/primus_aegis.cpython*.so)
 aegis_rt_so=${SCRIPT_DIR}/../build/lib/Runtime/FHE/libAegisRuntime.so.20.0git
+mlir_runner_units_so=${SCRIPT_DIR}/../third_party/llvm-project/build/lib/libmlir_c_runner_utils.so
+mlir_float16_units_so=${SCRIPT_DIR}/../third_party/llvm-project/build/lib/libmlir_float16_utils.so.20.0git
 onnx_mlir_exe=${SCRIPT_DIR}/../third_party/onnx-mlir/build/Release/bin/onnx-mlir
+aegis_compiler_exe=${SCRIPT_DIR}/../build/bin/aegiscompiler
 mlir_translate_exe=${SCRIPT_DIR}/../third_party/llvm-project/build/bin/mlir-translate
+mlir_runner_exe=${SCRIPT_DIR}/../third_party/llvm-project/build/bin/mlir-cpu-runner
 if [[ $OSTYPE == 'darwin'* ]]; then
   aegis_rt_so=${SCRIPT_DIR}/../build/lib/Runtime/FHE/libAegisRuntime.dylib
+  mlir_runner_units_so=${SCRIPT_DIR}/../third_party/llvm-project/build/lib/libmlir_c_runner_utils.dylib
+  mlir_float16_units_so=${SCRIPT_DIR}/../third_party/llvm-project/build/lib/libmlir_float16_utils.dylib
 fi
 for i in "${primus_aegis_so}" "${aegis_rt_so}" "${onnx_mlir_exe}" "${mlir_translate_exe}"; do
   if [ ! -f "${i}" ]; then
@@ -26,6 +32,19 @@ DIST_DIR=${SCRIPT_DIR}/../frontend/primus
 mkdir -p ${DIST_DIR}/bin ${DIST_DIR}/lib
 cp -f ${primus_aegis_so} ${DIST_DIR}/lib/
 cp -f ${aegis_rt_so} ${DIST_DIR}/lib/
+if [[ $OSTYPE == 'darwin'* ]]; then
+  cp -f ${mlir_runner_units_so} ${DIST_DIR}/lib/
+  cp -f ${mlir_runner_units_so} ${DIST_DIR}/lib/
+else
+  cp -fL ${mlir_runner_units_so} ${DIST_DIR}/lib/
+  cp -f ${mlir_float16_units_so} ${DIST_DIR}/lib/
+  cd ${DIST_DIR}/lib/
+  patchelf --remove-rpath libmlir_c_runner_utils.so
+  patchelf --set-rpath '$ORIGIN' libmlir_c_runner_utils.so
+
+fi
 cp -f ${onnx_mlir_exe} ${DIST_DIR}/bin/
+cp -f ${aegis_compiler_exe} ${DIST_DIR}/bin/
 cp -f ${mlir_translate_exe} ${DIST_DIR}/bin/
+cp -f ${mlir_runner_exe} ${DIST_DIR}/bin/
 chmod +x ${DIST_DIR}/bin/* ${DIST_DIR}/lib/*
